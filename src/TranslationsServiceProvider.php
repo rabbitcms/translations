ebc7c8047ae04c0452c21da2e693f30b70bb79aa
Dmytro Zhurakovskyi <dmytro.zhurakovsky@gmail.com>
2017-06-27 16:30:16 +0300

    added composer.json

---
+++ src/TranslationsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace RabbitCMS\Translations;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class TranslationsServiceProvider
+ *
+ * @package RabbitCMS\Translations
+ */
+class TranslationsServiceProvider extends ServiceProvider
+{
+
+}
